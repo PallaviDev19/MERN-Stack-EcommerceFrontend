45237fd4a561403fd03b442a5fa953572e960d22
Pallavi Devendra Patil <pallaviDev.patil@expleogroup.com>
2021-12-13 13:11:18 +0530

    link fix

--- src/api/constants_api.js
+++ src/api/constants_api.js
@@ -1,2 +1,4 @@
-const BASE_URL = "http://localhost:5000";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://ecommerce-recommen-engine.herokuapp.com";
 export default BASE_URL
+
